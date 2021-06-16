1fc40a8fa3537fd41fa502a4a80b2d3aa220e936
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-16 20:05:12 +0100

    use colorbit to display log on or off

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     Logging = false
+    colorbit_51bit.showColorIcon(ColorIcon.No, colorbit.colors(BitColors.Red))
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     Logging = true
 })
 let Logging = false
-let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
+let colorbit_51bit: colorbit.Strip = null
+colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.setBrightness(32)
 colorbit_51bit.clear()
-colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Green))
+colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Yellow))
 datalogger.setColumns(["Volume", "Light"])
 Logging = false
-loops.everyInterval(1000, function () {
+loops.everyInterval(200, function () {
     if (Logging) {
-        basic.showIcon(IconNames.Heart)
+        colorbit_51bit.showColorIcon(ColorIcon.Heart, colorbit.colors(BitColors.Red))
         datalogger.logData([datalogger.createCV("Volume", input.soundLevel()), datalogger.createCV("Light", input.lightLevel())])
-        basic.showIcon(IconNames.Heart)
+        colorbit_51bit.showColorIcon(ColorIcon.SmallHeart, colorbit.colors(BitColors.Red))
     }
 })
