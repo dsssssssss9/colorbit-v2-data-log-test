47f742852e335e4db9ac4017a44150169e3e4fc5
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-16 19:55:43 +0100

    use light level instead of magnetic

--- main.ts
+++ main.ts
@@ -8,13 +8,13 @@ let Logging = false
 let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.setBrightness(32)
 colorbit_51bit.clear()
-colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Blue))
-datalogger.setColumns(["Volume", "Magnetic"])
+colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Green))
+datalogger.setColumns(["Volume", "Light"])
 Logging = false
 loops.everyInterval(1000, function () {
     if (Logging) {
         basic.showIcon(IconNames.Heart)
-        datalogger.logData([datalogger.createCV("Volume", input.soundLevel()), datalogger.createCV("Magnetic", input.magneticForce(Dimension.X))])
+        datalogger.logData([datalogger.createCV("Volume", input.soundLevel()), datalogger.createCV("Light", input.lightLevel())])
         basic.showIcon(IconNames.Heart)
     }
 })
