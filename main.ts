9a8e66297c08f97b957a318b5f95322ddf96c761
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-16 19:52:03 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Logging = false
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Logging = true
+})
+let Logging = false
 let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.setBrightness(32)
 colorbit_51bit.clear()
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Blue))
 datalogger.setColumns(["Volume", "Magnetic"])
-let Logging = false
-basic.forever(function () {
-	
-})
+Logging = false
 loops.everyInterval(1000, function () {
     if (Logging) {
         basic.showIcon(IconNames.Heart)
