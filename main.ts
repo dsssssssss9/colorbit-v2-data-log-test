ffa0e066c72a6c6068c70e61285acc2fea35c463
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-16 19:50:51 +0100

    basic looging of volume & magnetic when logo touched press button a to stop

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
 let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.setBrightness(32)
 colorbit_51bit.clear()
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Blue))
+datalogger.setColumns(["Volume", "Magnetic"])
+let Logging = false
 basic.forever(function () {
 	
+})
+loops.everyInterval(1000, function () {
+    if (Logging) {
+        basic.showIcon(IconNames.Heart)
+        datalogger.logData([datalogger.createCV("Volume", input.soundLevel()), datalogger.createCV("Magnetic", input.magneticForce(Dimension.X))])
+        basic.showIcon(IconNames.Heart)
+    }
 })
